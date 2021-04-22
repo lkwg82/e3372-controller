5b240783fa6819a9550f58fe545b8f75a186510e
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-04-22 21:06:55 +0200

    adds empty list test

--- src/test/java/de/lgohlke/hilink/MappingTest.java
+++ src/test/java/de/lgohlke/hilink/MappingTest.java
@@ -1,8 +1,12 @@
 package de.lgohlke.hilink;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import de.lgohlke.API;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +26,22 @@ public class MappingTest {
 
         assertThat(o.code()).isEqualTo(125003);
         assertThat(o.message()).isEmpty();
+    }
+
+    @Test
+    void should_map_empty_list() throws JsonProcessingException {
+        var xml = """
+                <response>
+                    <Messages/>
+                </response>
+                """;
+        new XMLProcessor().readXml(xml, Response.class);
+    }
+
+    static class Response {
+        @JacksonXmlElementWrapper(localName = "Messages")
+        @JacksonXmlProperty(localName = "Message")
+        List<String> messages;
+
     }
 }
