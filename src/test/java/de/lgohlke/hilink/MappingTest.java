cf6e07bb8c100340604093db8e0384d033595485
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-04-23 01:10:23 +0200

    fix

--- src/test/java/de/lgohlke/hilink/MappingTest.java
+++ src/test/java/de/lgohlke/hilink/MappingTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MappingTest {
     @Test
     void map_error() throws JsonProcessingException, APIErrorException {
