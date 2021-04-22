0d6ecf6a0e26200324174b261259325f6f7058c9
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-04-22 21:06:24 +0200

    adds error test

--- src/main/java/de/lgohlke/hilink/XMLProcessor.java
+++ src/main/java/de/lgohlke/hilink/XMLProcessor.java
@@ -1,7 +1,5 @@
 package de.lgohlke.hilink;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -10,9 +8,6 @@ public class XMLProcessor {
     public <T> T readXml(String response, Class<T> clazz) throws JsonProcessingException {
         var mapper = new XmlMapper();
 
-        // see https://github.com/FasterXML/jackson-dataformat-xml/issues/124
-        mapper.configOverride(java.util.List.class)
-                .setSetterInfo(JsonSetter.Value.forValueNulls(Nulls.AS_EMPTY));
         try {
             return mapper.readValue(response, clazz);
         } catch (JsonProcessingException e) {
--- src/test/java/de/lgohlke/hilink/XMLProcessorTest.java
+++ src/test/java/de/lgohlke/hilink/XMLProcessorTest.java
@@ -3,14 +3,15 @@ package de.lgohlke.hilink;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.xmlunit.assertj3.XmlAssert.assertThat;
 
 public class XMLProcessorTest {
     private final XMLProcessor processor = new XMLProcessor();
+    private final TestObject test = new TestObject("test", 1);
 
     @Test
     void should_write_xml() throws JsonProcessingException {
-        var test = new TestObject("test", 1);
 
         var xml = processor.writeXml(test);
 
@@ -20,8 +21,6 @@ public class XMLProcessorTest {
 
     @Test
     void should_write_xml_with_indentation() throws JsonProcessingException {
-        var test = new TestObject("test", 1);
-
         var xml = processor.writeXml(test);
 
         var expected = """
@@ -35,8 +34,6 @@ public class XMLProcessorTest {
 
     @Test
     void should_write_xml_without_indentation() throws JsonProcessingException {
-        var test = new TestObject("test", 1);
-
         var xml = processor.writeXml(test, false);
 
         assertThat(xml).isEqualTo("<TestObject><name>test</name><age>1</age></TestObject>");
@@ -44,8 +41,6 @@ public class XMLProcessorTest {
 
     @Test
     void should_read_xml() throws JsonProcessingException {
-        var test = new TestObject("test", 1);
-
         var xml = """
                 <TestObject>
                   <name>test</name>
@@ -55,6 +50,18 @@ public class XMLProcessorTest {
         var actual = processor.readXml(xml, TestObject.class);
 
         assertThat(actual).isEqualTo(test);
+    }
+
+    @Test
+    void should_fail_read_xml() {
+        var xml = """
+                <TestObject>
+                  <name>test</name>
+                  <age>1</age>
+                  <agex>1</agex>
+                </TestObject>
+                """;
+        assertThatThrownBy(() -> processor.readXml(xml, TestObject.class)).isInstanceOf(JsonProcessingException.class);
     }
 
     record TestObject(String name, int age) {
